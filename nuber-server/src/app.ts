d9ff63cb323bae1c64df20a58bd322ecd0f57f6b
kanziw <kanziwoong@gmail.com>
2018-08-12 19:54:06 +0900

    [server] 모든 resolver 로 전달되는 context 샘플 작성

--- nuber-server/src/app.ts
+++ nuber-server/src/app.ts
@@ -1,4 +1,5 @@
 import cors from 'cors'
+import { NextFunction, Response } from 'express'
 import { GraphQLServer } from 'graphql-yoga'
 import helmet from 'helmet'
 import logger from 'morgan'
@@ -8,7 +9,14 @@ import decodeJWT from './utils/decodeJWT'
 class App {
   public app: GraphQLServer
   constructor() {
-    this.app = new GraphQLServer({ schema })
+    this.app = new GraphQLServer({
+      schema,
+      context: req => {
+        return {
+          req: req.request,
+        }
+      },
+    })
     this.middlewares()
   }
   private middlewares = (): void => {
@@ -18,11 +26,14 @@ class App {
     this.app.express.use(this.jwt)
   }
 
-  private jwt = async (req, res, next): Promise<void> => {
+  private jwt = async (
+    req,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> => {
     const token = req.get('X-JWT')
     if (token) {
-      const user = await decodeJWT(token)
-      console.log(user)
+      req.user = await decodeJWT(token)
     }
     next()
   }
