ca21b589626284f5242750ca15fdad64b69710e5
David Jung <david.jung@peeper.co>
2018-08-20 19:16:16 +0900

    [client] local query 구문을 codegen 명령어에서 제외, 인증화면에 state 로 phone 정보 전달

--- nuber-client/src/types/api.d.ts
+++ nuber-client/src/types/api.d.ts
@@ -6,17 +6,40 @@
 // ====================================================
 
 export interface startPhoneVerification_StartPhoneVerification {
-  __typename: "StartPhoneVerificationResponse";
-  ok: boolean;
-  error: string | null;
+  __typename: 'StartPhoneVerificationResponse'
+  ok: boolean
+  error: string | null
 }
 
 export interface startPhoneVerification {
-  StartPhoneVerification: startPhoneVerification_StartPhoneVerification;
+  StartPhoneVerification: startPhoneVerification_StartPhoneVerification
 }
 
 export interface startPhoneVerificationVariables {
-  phoneNumber: string;
+  phoneNumber: string
+}
+
+/* tslint:disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL mutation operation: verifyPhone
+// ====================================================
+
+export interface verifyPhone_CompletePhoneVerification {
+  __typename: 'CompletePhoneVerificationResponse'
+  ok: boolean
+  error: string | null
+  token: string | null
+}
+
+export interface verifyPhone {
+  CompletePhoneVerification: verifyPhone_CompletePhoneVerification
+}
+
+export interface verifyPhoneVariables {
+  key: string
+  phoneNumber: string
 }
 
 /* tslint:disable */
