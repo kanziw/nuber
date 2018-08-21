fe08213cb98bbd7f29042abac28e88c33ba1c3fc
David Jung <david.jung@peeper.co>
2018-08-21 12:54:32 +0900

    [client] GoogleMap Module import
    * npm i google-maps-react
    * npm i --save-dev @types/googlemaps

--- nuber-client/src/Routes/AddPlace/AddPlacePresenter.tsx
+++ nuber-client/src/Routes/AddPlace/AddPlacePresenter.tsx
@@ -28,6 +28,7 @@ interface IProps {
   onInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void
   loading: boolean
   onSubmit: MutationFn
+  pickedAddress: boolean
 }
 
 const AddPlacePresenter: React.SFC<IProps> = ({
@@ -36,6 +37,7 @@ const AddPlacePresenter: React.SFC<IProps> = ({
   name,
   loading,
   onSubmit,
+  pickedAddress,
 }) => (
   <React.Fragment>
     <Helmet>
@@ -59,7 +61,12 @@ const AddPlacePresenter: React.SFC<IProps> = ({
           name={'address'}
         />
         <ExtendedLink to={'/find-address'}>Pick place from map</ExtendedLink>
-        <Button onClick={null} value={loading ? 'Adding place' : 'Add Place'} />
+        {pickedAddress && (
+          <Button
+            onClick={null}
+            value={loading ? 'Adding place' : 'Add Place'}
+          />
+        )}
       </Form>
     </Container>
   </React.Fragment>
