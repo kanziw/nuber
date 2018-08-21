c756a25b7c459e35d1f133303819e0ac04148813
David Jung <david.jung@peeper.co>
2018-08-21 10:43:16 +0900

    [client] Facebook 소셜 로그인기능 추가 및 Login 성공 데이터 핸들링
    * npm i react-facebook-login

--- nuber-client/src/types/api.d.ts
+++ nuber-client/src/types/api.d.ts
@@ -6,17 +6,42 @@
 // ====================================================
 
 export interface startPhoneVerification_StartPhoneVerification {
-  __typename: 'StartPhoneVerificationResponse'
-  ok: boolean
-  error: string | null
+  __typename: "StartPhoneVerificationResponse";
+  ok: boolean;
+  error: string | null;
 }
 
 export interface startPhoneVerification {
-  StartPhoneVerification: startPhoneVerification_StartPhoneVerification
+  StartPhoneVerification: startPhoneVerification_StartPhoneVerification;
 }
 
 export interface startPhoneVerificationVariables {
-  phoneNumber: string
+  phoneNumber: string;
+}
+
+/* tslint:disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL mutation operation: facebookConnect
+// ====================================================
+
+export interface facebookConnect_FacebookConnect {
+  __typename: "FacebookConnectResponse";
+  ok: boolean;
+  error: string | null;
+  token: string | null;
+}
+
+export interface facebookConnect {
+  FacebookConnect: facebookConnect_FacebookConnect;
+}
+
+export interface facebookConnectVariables {
+  firstName: string;
+  lastName: string;
+  email?: string | null;
+  fbId: string;
 }
 
 /* tslint:disable */
@@ -27,19 +52,19 @@ export interface startPhoneVerificationVariables {
 // ====================================================
 
 export interface verifyPhone_CompletePhoneVerification {
-  __typename: 'CompletePhoneVerificationResponse'
-  ok: boolean
-  error: string | null
-  token: string | null
+  __typename: "CompletePhoneVerificationResponse";
+  ok: boolean;
+  error: string | null;
+  token: string | null;
 }
 
 export interface verifyPhone {
-  CompletePhoneVerification: verifyPhone_CompletePhoneVerification
+  CompletePhoneVerification: verifyPhone_CompletePhoneVerification;
 }
 
 export interface verifyPhoneVariables {
-  key: string
-  phoneNumber: string
+  key: string;
+  phoneNumber: string;
 }
 
 /* tslint:disable */
