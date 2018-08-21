13e7c0d49c72ab036936d3f9c60a23904e49e3c9
David Jung <david.jung@peeper.co>
2018-08-21 14:19:53 +0900

    [client] 지도화면에 주소 표시, 위/경도 데이터를 실제 address 로 받아오는 기능 추가
    * npm i --save-dev @types/axios

--- nuber-client/src/Routes/FindAddress/FindAddressContainer.tsx
+++ nuber-client/src/Routes/FindAddress/FindAddressContainer.tsx
@@ -1,27 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { reverseGeoCode } from '../../mapHelpers'
 import FindAddressPresenter from './FindAddressPresenter'
 
 interface IState {
   lat: number
   lng: number
+  address: string
 }
 
 class FindAddressContainer extends React.Component<any, IState> {
   public mapRef: any
   public map: google.maps.Map
+  public state = {
+    address: '',
+    lat: 0,
+    lng: 0,
+  }
+
   constructor(props) {
     super(props)
     this.mapRef = React.createRef()
   }
+
   public componentDidMount() {
     navigator.geolocation.getCurrentPosition(
       this.handleGeoSucces,
       this.handleGeoError,
     )
   }
+
   public render() {
-    return <FindAddressPresenter mapRef={this.mapRef} />
+    const { address } = this.state
+    return (
+      <FindAddressPresenter
+        mapRef={this.mapRef}
+        address={address}
+        onInputChange={this.onInputChange}
+        onInputBlur={this.onInputBlur}
+      />
+    )
   }
 
   public handleGeoSucces = (positon: Position) => {
@@ -55,14 +73,29 @@ class FindAddressContainer extends React.Component<any, IState> {
     this.map.addListener('dragend', this.handleDragEnd)
   }
 
-  public handleDragEnd = () => {
+  public handleDragEnd = async () => {
     const newCenter = this.map.getCenter()
     const lat = newCenter.lat()
     const lng = newCenter.lng()
+    const reversedAddress = await reverseGeoCode(lat, lng)
     this.setState({
+      address: reversedAddress,
       lat,
       lng,
     })
+  }
+
+  public onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = event
+    this.setState({
+      [name]: value,
+    } as any)
+  }
+
+  public onInputBlur = () => {
+    console.log('Address updated')
   }
 }
 
